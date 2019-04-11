e0b8d69e1ef8e64b8e3ea62a1990b68af129aad4
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-11 16:02:12 +0700

    add code on index.js to define the area of the triangle

---
+++ index.js
@@ -0,0 +1,7 @@
+//formula of area of triangle
+//area of triangle = 1/2 * base of triangle * height of triangle
+const triangleBase = 50;
+const triangleHeight = 10;
+const areaOfTriangle = 0.5 * triangleBase * triangleHeight;
+
+console.log(areaOfTriangle);
